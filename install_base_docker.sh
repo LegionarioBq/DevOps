80ed66091083e02c99cd64124548eb4f25ed5bf2
Albert <git>
2025-04-10 18:45:20 -0300

    Feat: criando arquivo de instalação remoto para base docker e atualizar ubuntu serve

---
+++ install_base_docker.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Script Instalar e atualizar base load balance
+
+echo "🕒 Atualizando data e fuso horário..."
+date
+sudo timedatectl set-timezone America/Sao_Paulo
+timedatectl
+date
+
+echo "🛠️ Atualizando sistema..."
+sudo apt update && sudo apt upgrade -y
+sudo apt autoremove -y
+sudo apt autoclean
+
+echo "🐳 Instalando Docker (última versão)..."
+sudo apt install -y ca-certificates curl gnupg lsb-release
+
+# Adicionando chave oficial do Docker
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
+
+# Adicionando repositório oficial Docker
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Atualizando e instalando Docker
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Habilita e inicia o serviço Docker e containerd para iniciar automaticamente com o sistema
+sudo systemctl enable docker
+sudo systemctl enable containerd
+sudo systemctl start docker
+
+echo "✅ Docker instalado e sistema atualizado com sucesso!"
+docker --version
